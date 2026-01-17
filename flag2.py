e27b1a0ae1422af7e2d9bd0b39ed248b89167897
Raees chishty <mohommedraees@gmail.com>
2026-01-17 16:00:05 +0530

    Add flag2 script

---
+++ flag2.py
@@ -0,0 +1,60 @@
+"""
+Docstring for flag2
+
+"""
+import collections 
+import requests 
+import tqdm 
+from flag2_common import main,save_flag,HTTPStatus,Result 
+
+Default_concur_req=1 
+Max_concur_req=1 
+
+def get_flag(base_url,cc):
+    url='{}/{cc}/{cc}.gif'.format(base_url,cc=cc.lower())
+    resp=requests.get(url)
+    if resp.status_code!=200:
+        resp.raise_for_status()
+    return resp.content()
+def download_one(cc,base_url,verbose=False):
+    try:
+        image=get_flag(base_url,cc)
+    except requests.exceptions.HTTPError as exc:
+        res=exc.response
+        if res.status_code==404:
+            status=HTTPStatus.not_found 
+            msg='not found😁'
+        else:
+            raise
+    else: 
+        save_flag(image,cc.lower()+'.gif')
+        status=HTTPStatus.ok 
+        msg='OK🙄'
+    if verbose:
+        print(cc,msg)
+    return Result(status,cc)
+def download_many(cc_list,base_url,verbose,max_req):
+    counter=collections.Counter()
+    cc_iter=sorted(cc_list)
+    if not verbose:
+        cc_iter=tqdm.tqdm(cc_iter)
+    for cc in cc_iter:
+        try:
+            res=download_one(cc,base_url,verbose)
+        except requests.exceptions.HTTPError as exc:
+            error_msg='HTTP error {res.status_code} - {res.reason}'
+            error_msg=error_msg.format(res=exc.response)
+        except requests.exceptions.ConnectionError as exc:
+            error_msg='Connection errro🤔'
+        else:
+            error_msg=''
+            status=res.status 
+        if error_msg:
+            status=HTTPStatus.error
+        counter[status]+=1 
+        if verbose and error_msg:
+            print('*** Error for {}: {}'.format(cc,error_msg))
+    return counter 
+if __name__=='__main__':
+    main(download_many,Default_concur_req,Max_concur_req)
+
